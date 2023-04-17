151f12d6c6e2d6a20a7262ee97557b1da986d5aa
Vaishnavi  Gomkale <109347851+gomkalevaishnavi@users.noreply.github.com>
2023-04-17 09:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // complete this js code
 function Person(name, age) {
-	constructor(name, age){
-		this._name = name;
-		this._age = age;
-	}
+	this.name = name;
+  this.age = age;
+}
 	Person.prototype.greet = function(){
 		console.log('Hello, my name is ${this.name} , I am ${this.age} years old.');
 		
 	};
-}
+
 
 function Employee(name, age, jobTitle) {
 Person.call(this, name, age);
